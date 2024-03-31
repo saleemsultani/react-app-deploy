c87ed52b9c3f436059244dd6bb43c35998b22586
saleem sultani <69337218+saleemsultani@users.noreply.github.com>
2024-03-31 15:29:46 +0200

    Update

--- server.js
+++ server.js
@@ -8,20 +8,25 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 // Config .env
 dotenv.config();
 
+// connect Database
+connectDB();
+
+// esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")));
-
-// connect Database
-connectDB();
 
 //routes
 app.use("/api/v1/auth", authRoutes);
